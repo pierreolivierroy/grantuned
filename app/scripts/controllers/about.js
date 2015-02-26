87dc2b730abc91e28b9238c7c3d9a56cb2b833ca
pierreolivierroy <roy.pierre.o@gmail.com>
2015-02-26 13:21:46 -0500

    Création de la structure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name grantunedApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the grantunedApp
+ */
+angular.module('grantunedApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name grantunedApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the grantunedApp
+ */
+angular.module('grantunedApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
