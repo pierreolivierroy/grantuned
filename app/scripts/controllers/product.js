237a4b0ccd3cd867ae134f897f5506617d009561
pierreolivierroy <roy.pierre.o@gmail.com>
2015-05-31 22:13:47 -0400

    Specifications

--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -16,17 +16,31 @@ angular.module('grantunedApp')
 
     $scope.specs = [
       {
-        title: 'Aerodynamics', items: [ { name: 'Active rear wings' }, { name: 'Active independant front flaps' }, { name: 'Frontal area: 1.873 m' } ]
+        title: 'Aerodynamics',
+        items: [ { name: 'Active rear wings' }, { name: 'Active independant front flaps' },
+          { name: 'Frontal area: 1.873 m^2' }, { name: 'Total down force at 250 km/h 485kg' } ]
       },
       {
-        title: 'Dimensions', items: [ { name: 'Total length: 4293 mm' }, { name: 'Total width: 2050 mm' } ]
+        title: 'Engine',
+        items: [ { name: 'Koenigsegg twin turbo aluminum 5,0L V8, 4 valves per cylinder, double overhead camshafts, dry sump lubrification' },
+          { name: 'Compression: 9.3:1' }, { name: 'Bore: 92 mm' }, { name: 'Stroke: 95.25 mm' },
+          { name: 'Sequential, multipoint fuel injection with dual knock sensors and back pressure' } ]
       },
       {
-        title: 'Chassis', items: [ { name: 'Carbon fiber with aluminium honeycomb' } ]
+        title: 'Dimensions',
+        items: [ { name: 'Total length: 4293 mm' }, { name: 'Total width: 2050 mm' }, { name: 'Total height: 1120 mm' },
+          { name: 'Low speed ground clearance: 105 mm' }, { name: 'Track mode ground clearance: 85 mm' },
+          { name: 'Front lifting system activated: 150 mm' }, { name: 'Wheelbase: 2662 mm' }, { name: 'Fuel capacity: 150 litres' },
+          { name: 'Luggage compartment: 150 litres' }, { name: 'Dry weight: 1295 kg (Curb weight: 1395 kg)' } ]
       },
       {
-        title: 'Engine', items: [ { name: 'Koenigsegg twin turbo aluminum 5,0L V8' } ]
+        title: 'Chassis',
+        items: [ { name: 'Carbon fiber with aluminium honeycomb' }, { name: 'Carbon fiber with aluminum honeycomb and integrated fuel tanks' },
+          { name: 'Monocoque torsional rigidity: 65,000 Nm/degree' }, { name: 'Weight: 70 kg (including tanks)' },
+          { name: 'Front and rear suspension - double wishbones, carbon fiber rear upper wish-bones, two-way electronically adjustable gas-hydraulic shock absorbers, and pushrod operated Triplex damper in the rear' },
+          { name: 'Electronically adjustable ride height, fully independent per wheel' } ]
       }
+
     ];
 
     $scope.images = [
